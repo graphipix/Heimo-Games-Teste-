d259481c93f4d13ac325a2d9b3afd917999c09f2
graphipix <graphipix.contact@gmail.com>
2024-01-23 12:41:36 -0300

    Car Controller Implementation
    Implementando controle do carro ( Giro e Direção da Roda)

--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -5,20 +5,48 @@ using UnityEngine;
 public class CarController : MonoBehaviour
 {
     Rigidbody rgbd;
-    [SerializeField] [Range(10,100)] int Force = default;
+    [SerializeField] [Range(10, 100)] int Force = default;
     Vector3 IsMoving = default;
 
     private void Start()
     {
         rgbd = GetComponent<Rigidbody>();
     }
-
-    private void Update()
-    {
-    }
-
     private void FixedUpdate()
     {
-        rgbd.velocity = new Vector3(Input.GetAxis("Horizontal") * Force, 0, Input.GetAxis("Vertical")* Force);
+        rgbd.velocity = new Vector3(Input.GetAxis("Horizontal") * Force, 0, Input.GetAxis("Vertical") * Force);
+        WheelController();
     }
+    // Settings
+    [SerializeField] private float motorForce, breakForce, maxSteerAngle;
+
+    // Wheel Colliders
+    [SerializeField] private WheelCollider FrontLeftWheelCollider, FrontRightWheelCollider;
+    [SerializeField] private WheelCollider rearLeftWheelCollider, rearRightWheelCollider;
+
+    // Wheels
+    [SerializeField] private Transform FrontLeftWheel, FrontRightWheel = default;
+    [SerializeField] private Transform BackLeftWheel, BackRightWheel = default;
+
+    #region WheelController
+
+    public int axis;
+    private void WheelController()
+    {
+        FrontLeftWheel.rotation = Quaternion.Euler(axis, 30 * Input.GetAxis("Horizontal"), transform.rotation.z);
+        FrontRightWheel.rotation = Quaternion.Euler(axis, 30 * Input.GetAxis("Horizontal"), transform.rotation.z);
+        BackLeftWheel.rotation = Quaternion.Euler(axis *-1, 30 * Input.GetAxis("Horizontal"), transform.rotation.z);
+        BackRightWheel.rotation = Quaternion.Euler(axis * -1, 30 * Input.GetAxis("Horizontal"), transform.rotation.z);
+
+        transform.rotation = Quaternion.Euler(Input.GetAxis("Horizontal"),transform.rotation.y,transform.rotation.z);
+
+        //Wheel Axis Rotation
+        if (Input.GetAxis("Vertical") != 0)
+        {
+            axis += 45;
+        }
+
+
+    }
+    #endregion
 }
